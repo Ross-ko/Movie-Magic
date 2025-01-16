778c8ec23ce006041980d699b819e5934ee89c2a
Rosen Ignatov <rosen.ignatoff@gmail.com>
2025-01-16 21:11:56 +0200

    instal and setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('it works');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Its working');
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
