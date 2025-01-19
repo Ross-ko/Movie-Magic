83ff34dbfe27deb6862ac98ac8abd9e34baff027
Rosen Ignatov <rosen.ignatoff@gmail.com>
2025-01-19 17:00:31 +0200

    Add getAll to movie service

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
-
-import movies from '../movies.js'; //temporary solution
+import movieService from '../services/movie-service.js';
 
 const router = Router();
 
 router.get('/', (req, res) => {
+    const movies = movieService.getAll();
+    
     res.render('home', {movies});
 });
 
--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -2,6 +2,10 @@ import movies from "../movies.js";
 import {v4 as uuid} from 'uuid';
 
 export default {
+  getAll() {
+    return movies;
+  },
+  
   findOne(movieId) {
     const result = movies.find((movie) => movie.id === movieId);
 
