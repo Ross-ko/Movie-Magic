cc44130ee6d28c75f96b74a7e6cb6413e96fbfd8
Rosen Ignatov <rosen.ignatoff@gmail.com>
2025-01-19 16:30:52 +0200

    parse form data on server (new movie)

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -6,6 +6,13 @@ const movieController = Router();
 movieController.get('/create', (req, res) => {
     res.render('create');
 });
+movieController.post('/create', (req, res) => {
+    const newMovie = req.body;
+    
+    console.log(req.body);
+
+    res.end();
+});
 
 movieController.get('/:movieId/details', (req, res) => {
     const movieId = req.params.movieId;
