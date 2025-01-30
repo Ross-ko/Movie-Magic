970e532cb467b94239be7c7f8259fbd716f8bbbd
Rosen Ignatov <rosen.ignatoff@gmail.com>
2025-01-30 19:12:24 +0200

    exclude casts that are already attached

--- src/services/cast-service.js
+++ src/services/cast-service.js
@@ -1,8 +1,16 @@
 import Cast from "../models/Cast.js";
 
 export default {
-    getAll(идс) {
+    getAll(filter = {}) {
         // return Cast.find({_id: {$in: ids}}); - дърводелската
+        let query = Cast.find({});
+
+        if (filter.exclude) {
+            query = query.find({_id: {$nin: filter.exclude}})
+        }
+
+        return query;
+
         return Cast.find({});
     },
 
